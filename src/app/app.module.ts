591459ade46f5874e69db5b728cbb8e2f29c5336
Roy OKite <royokite@gmail.com>
2023-05-12 13:00:46 +0300

    :sparkles: Add table and inject data from api
    Add the table to the page.
    Inject data from the fake RESTapi.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { CollectionsComponent } from './pages/collections/collections.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/collections/collections.component.ts
+++ src/app/pages/collections/collections.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CollectionsapiService } from 'src/app/services/collectionsapi.service';
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.css']
 })
-export class CollectionsComponent {
+
+export class CollectionsComponent implements OnInit {
+
+  constructor(private service:CollectionsapiService) {}
+
+  ngOnInit(): void {
+      this.getAllCollections();
+  }
+
+  displayedColumns = ['position', 'collectionCode', 'farmer', 'collectionSize', 'collectionDate'];
+
+  collectionsData: any;
+
+  getAllCollections() {
+    this.service.getAllCollections().subscribe(response => {
+      this.collectionsData = response;
+    });
+  }
+
 
 }
