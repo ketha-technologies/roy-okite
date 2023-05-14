e596571b1541b59a00c306c0eef737a6868eb00a
Roy OKite <royokite@gmail.com>
2023-05-14 10:31:49 +0300

    :sparkles: Add routing
    Set up routes for the collections and chart components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSortModule } from '@angular/material/sort'
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 import { ChartComponent } from './pages/chart/chart.component';
 
+const appRoute: Routes = [
+  {path: '', redirectTo: 'collections', pathMatch: 'full'},
+  {path: 'collections', component: CollectionsComponent},
+  {path: 'chart', component: ChartComponent},
+]
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +34,8 @@ import { ChartComponent } from './pages/chart/chart.component';
     MatFormFieldModule,
     MatInputModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [provideAnimations()],
   bootstrap: [AppComponent]
