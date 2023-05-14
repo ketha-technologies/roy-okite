65a29e92908cb81154e5df42e7463b9fa2c40d74
Roy OKite <royokite@gmail.com>
2023-05-14 11:20:58 +0300

    :recycle: Move route logic to routing module
    Move routing to appropriate routing module file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { ChartComponent } from './pages/chart/chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'collections', pathMatch: 'full'},
+  {path: 'collections', component: CollectionsComponent},
+  {path: 'chart', component: ChartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSortModule } from '@angular/material/sort'
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CollectionsComponent } from './pages/collections/collections.component';
 import { ChartComponent } from './pages/chart/chart.component';
 
-const appRoute: Routes = [
-  {path: '', redirectTo: 'collections', pathMatch: 'full'},
-  {path: 'collections', component: CollectionsComponent},
-  {path: 'chart', component: ChartComponent},
-]
- 
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +28,6 @@ const appRoute: Routes = [
     MatInputModule,
     MatPaginatorModule,
     MatSortModule,
-    RouterModule.forRoot(appRoute)
   ],
   providers: [provideAnimations()],
   bootstrap: [AppComponent]
